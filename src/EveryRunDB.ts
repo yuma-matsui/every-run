3146a401a47dd5bb8c228b9b4b9a29ec315962f1
yuma-matsui <oshino.iiyo@gmail.com>
2022-07-23 11:14:41 +0900

    EveryRunDBクラスの雛形作成

---
+++ src/EveryRunDB.ts
@@ -0,0 +1,33 @@
+import * as sqlite3 from 'sqlite3'
+import type { sqlite3 as Sqlite3 } from 'sqlite3'
+
+export class EveryRunDB {
+  static storage = './every_run.db'
+  static createRunnerTableStatement = 'CREATE TABLE if not exists Runner (id INTEGER PRIMARY KEY AUTOINCREMENT, distance INTEGER)'
+  static createRunningLogTableStatement = 'CREATE TABLE if not exists RunningLog (id INTEGER PRIMARY KEY AUTOINCREMENT, distance INTEGER, date TEXT)'
+
+  #db: sqlite3.Database
+  constructor () {
+    const sqlite: Sqlite3 = sqlite3.verbose()
+    this.#db = new sqlite.Database(EveryRunDB.storage)
+    this.#createTable(EveryRunDB.createRunnerTableStatement)
+    this.#createTable(EveryRunDB.createRunningLogTableStatement)
+  }
+
+  close () {
+    this.#db.close()
+  }
+
+  #createTable (createTableStatement: string) {
+    const method = () => {
+      this.#db.run(createTableStatement)
+    }
+    this.#serialize(method)
+  }
+
+  #serialize (method: (callback?: (() => void) | undefined) => void) {
+    this.#db.serialize(() => {
+      method()
+    })
+  }
+}
